f815eb5b85c50e9e596e21ae3b3bb0ca289575b0
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-06-18 19:42:56 +0900

    validate argument length
    [BREAKING] drop support for Node.js < 6

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 'use strict';
 
-const join = require('path').join;
+const {join} = require('path');
 
 const npmCliDir = require('npm-cli-dir');
 
-module.exports = function npmCliVersion() {
+module.exports = function npmCliVersion(...args) {
+  const argLen = args.length;
+
+  if (argLen !== 0) {
+    return Promise.reject(new RangeError(`Expected no arguments, but got ${argLen} argument${
+      argLen === 1 ? '' : 's'
+    }.`));
+  }
+
   return npmCliDir().then(dirPath => {
-    return require(join(dirPath, './package.json')).version;
+    return require(join(dirPath, 'package.json')).version;
   });
 };
