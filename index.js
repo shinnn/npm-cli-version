3e322faf8364c98c7937f931be8a7f7f1f7903ab
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-06-20 19:42:56 +0900

    use async/await to simplify the code
    [BREAKING] drop suppor for Node.js < 7

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ const {join} = require('path');
 
 const npmCliDir = require('npm-cli-dir');
 
-module.exports = function npmCliVersion(...args) {
+module.exports = async function npmCliVersion(...args) {
   const argLen = args.length;
 
   if (argLen !== 0) {
-    return Promise.reject(new RangeError(`Expected no arguments, but got ${argLen} argument${
+    throw new RangeError(`Expected no arguments, but got ${argLen} argument${
       argLen === 1 ? '' : 's'
-    }.`));
+    }.`);
   }
 
-  return npmCliDir().then(dirPath => {
-    return require(join(dirPath, 'package.json')).version;
-  });
+  return require(join(await npmCliDir(), 'package.json')).version;
 };
